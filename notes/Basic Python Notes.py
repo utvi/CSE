37717135b89b1b76b1d8557e27c8d48e88005346
utvi <utvi@fresnou.org>
2018-11-28 08:03:42 -0800

    notes

--- notes/Basic Python Notes.py
+++ notes/Basic Python Notes.py
@@ -93,33 +93,117 @@ For loops - Use when you know EXACTLY how many iterations
 While loops - Use when you DON'T know how many iterations
 '''
 
-# Random numbers
-import random  # This should always be on line 1
-print(random.randint(0, 100))
-
-# Control Statements
-def grade_calc(percentage):
-    if percentage >= 90:
-        return "A"
-    elif percentage >= 80:
-        return "B"
-    elif percentage >= 60:
-        return "C"
-    elif percentage >= 70:
-        return "D"
-    else:
-        return "F"
-
-
-print(grade_calc())
-
-# Equality Statements
-print(5 > 3)
-print(5 >= 3)
-print(3 == 3)
-print(3 != 4)
+# # Random numbers
+# import random  # This should always be on line 1
+# print(random.randint(0, 100))
+#
+# # Control Statements
+# def grade_calc(percentage):
+#     if percentage >= 90:
+#         return "A"
+#     elif percentage >= 80:
+#         return "B"
+#     elif percentage >= 60:
+#         return "C"
+#     elif percentage >= 70:
+#         return "D"
+#     else:
+#         return "F"
+#
+#
+# print(grade_calc())
+#
+# # Equality Statements
+# print(5 > 3)
+# print(5 >= 3)
+# print(3 == 3)
+# print(3 != 4)
 
 '''
 a = 3 # A is set to 3
 a == 3 # Is equal to 3?
 '''
+
+# Lists
+shopping_list = ["milk", "PC", "Eggs", "Xbox One", "PS4"]
+print(shopping_list)
+print(shopping_list[0])
+print("The second thing in the list is %s" % shopping_list[1])
+print("The length of the list is %d" % len(shopping_list))
+
+# Changing Elements in a list
+shopping_list[0] = "milk"
+print(shopping_list)
+print(shopping_list[0])
+
+# Looping through lists
+for item in shopping_list :
+    print(item)
+
+'''
+1. Make a list
+2. change the third thing in the list
+3. print the item
+4.print the list
+'''
+
+new_list = ["egg", "cheese", "oranges", "raspberries"]
+new_list[2] = "apples"
+print("The last thing in the list is %s" % new_list[len(new_list) -1])
+print(new_list)
+
+# Getting part of a list
+print(new_list[1:3])
+print(new_list[1:4])
+print(new_list[1:])
+print(new_list[:2])
+
+# Adding things to the list
+christmas_list = []  # ALWASY USE SQUARE BRACKETS
+christmas_list.append("Tacos")
+christmas_list.append("Bumblebee")
+christmas_list.append("Red Dead Redemption 2")
+print(christmas_list)
+
+# Noice this is an "object.method(Parameters)"
+
+# Removing things from a list
+christmas_list.remove("Tacos")
+print(christmas_list)
+
+'''
+1. make a new list with 3 items
+2. add a 4th item to the list
+3. remove one of the first three itms from the list
+'''
+practice_list = ["chair", "bacon", "bear"]
+practice_list.append("hairspray")
+practice_list.remove("bacon")
+print(practice_list)
+
+# also removing things from a list
+christmas_list.pop(0)  # removes the item at index 0
+print(christmas_list)
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet',
+          'purple', 'black', 'grey', 'brown', 'gold', 'pink', 'cyan',
+          'white', 'turquoise']
+print(len(colors))
+
+# find the index
+print(colors.index("gold"))
+
+# changing things into a list
+string1 = "turquoise"
+list1 = list(string1)
+print(list1)
+
+for character in list1:
+    if character == "u":
+        # replace with a *
+        current_index = list1.index(character)
+        list1.pop(current_index)
+        list1_insert(current_index, "*")
+
+# changing lists into strings
+print("".join(list1))
