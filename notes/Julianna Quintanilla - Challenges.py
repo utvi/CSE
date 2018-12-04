c9b3c7bcac6a86af09ea603032d9bacce5a66d8d
utvi <utvi@fresnou.org>
2018-12-04 08:42:46 -0800

    challeges

---
+++ notes/Julianna Quintanilla - Challenges.py
@@ -0,0 +1,12 @@
+#Challenge 1
+print("What is your first name? ")
+First_name = input()
+print("What is your last name? ")
+Last_name = input()
+print(Last_name, First_name)
+
+# Challenge 2
+print("What is your number? ")
+num = input()
+if num % 2:
+    print("This number is even")
