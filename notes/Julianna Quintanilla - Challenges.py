9f9394b19bd01f7b727ef04627866655bbf00f4f
utvi <utvi@fresnou.org>
2018-12-06 08:42:13 -0800

    challenges

--- notes/Julianna Quintanilla - Challenges.py
+++ notes/Julianna Quintanilla - Challenges.py
@@ -1,12 +1,47 @@
-#Challenge 1
-print("What is your first name? ")
-First_name = input()
-print("What is your last name? ")
-Last_name = input()
-print(Last_name, First_name)
+# Challenge 1
+def challenge1(first_name, last_name):
+    print(last_name, first_name)
+
+
+challenge1("John", "Doe")
 
 # Challenge 2
-print("What is your number? ")
-num = input()
-if num % 2:
-    print("This number is even")
+
+
+def challenge2(number1):
+    if number1 % 2 == 0:
+        print("This number is even")
+    else:
+        print("This number is odd")
+
+
+challenge2(4)
+challenge2(79)
+challenge2(879498765687654987654687870)
+
+# Challenge 3
+
+
+def challenge3(base, height):
+    print(base*height/2)
+
+
+challenge3(5, 3)
+challenge3(9, 2)
+
+# Challenge 4
+
+
+def challenge4(number2):
+    if number2 >= 0:
+        print("This number is positive.")
+    elif number2 == 0:
+        print("This number is zero")
+    elif number2 <= 0:
+        print("This number is negative")
+
+challenge4(-9)
+challenge4(8)
+challenge4(0)
+
+# Challenge 5
