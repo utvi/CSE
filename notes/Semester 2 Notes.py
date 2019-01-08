5bf4f99342d9b074e7a9953e4c823eb16aa85899
utvi <42383766+utvi@users.noreply.github.com>
2019-01-08 08:43:25 -0800

    notes 2

---
+++ notes/Semester 2 Notes.py
@@ -0,0 +1,19 @@
+print("hello world")
+# single-line comment
+cars = 5
+driving = True
+# cars = 5 = "5"
+
+
+print("I have %d cars" % cars)
+
+age = input("How old are you? ")
+print ("Wow %d, you're old." % age)
+
+
+colors = ["red", "orange", "yellow", "green", "blue"]
+colors.append("brown")
+
+print(colors)
+colors.pop(0)
+print(colors)
