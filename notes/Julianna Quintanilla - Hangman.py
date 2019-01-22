e1b8283c10af7a9fd1b66898b9b1d6127df290a5
utvi <42383766+utvi@users.noreply.github.com>
2019-01-22 08:43:30 -0800

    hangman

--- notes/Julianna Quintanilla - Hangman.py
+++ notes/Julianna Quintanilla - Hangman.py
@@ -1,10 +1,24 @@
+#    import string
+#    print(list(string.ascii_letters))
+#    print(string.digits)
+#    print(string.punctuation)
 import random
-word_bank = ["Mailman", "lemon", "speaker", "peach", "Tricycle", "night",
-             "I have a red bike.", "yellow", "Mothman", "hippopotamus"]
+word_bank = ["mailman", "lemon", "speaker", "peach", "tricycle",
+             "night", "bike", "yellow", "mothman", "hippopotamus"]
 turns_taken = 0
 remaining_guesses = 8
 letters_used = []
 correct_guesses = []
+wrong_guesses = []
 chosen_word = word_bank[random.randint(0, 9)]
+chosen_word = chosen_word.lower()
 for i in range(0, len(chosen_word)):
-    correct_guesses.append("*")
+    word_bank.append("*"*len(chosen_word))
+    chosen_word = "*" * len(chosen_word)
+#   letters_used = input("Guess a letter. ")
+#    if letters_used in chosen_word:
+#    if letter.upper() == _.upper():
+#   chosen_word = "*"*len(chosen_word)
+print(chosen_word)
+letters_used = input("Guess a letter. ")
+
