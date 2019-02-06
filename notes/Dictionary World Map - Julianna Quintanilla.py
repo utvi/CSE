3aef1064bd0c42d379ad6767dc57e5184294e377
utvi <42383766+utvi@users.noreply.github.com>
2019-02-06 08:41:58 -0800

    WORLD MAP

--- notes/Dictionary World Map - Julianna Quintanilla.py
+++ notes/Dictionary World Map - Julianna Quintanilla.py
@@ -8,14 +8,50 @@ world_map = {
         }
     },
     'PARKING_LOT': {
-        'NAME': "The Edison Parking Lot",
+        'NAME': "The Parking Lot",
         'DESCRIPTION': "There are cars parked here. To "
                        "the south is Mr. Wiebe's room",
         'PATHS': {
-            'SOUTH': "R19A"
-            ''
+            'SOUTH': "R19A",
+            'EAST': "ART_BUILDING",
+            'WEST': "HALLWAY"
         }
-    }
+    },
+    'ART_BUILDING': {
+        'NAME': "Art Building",
+        'DESCRIPTION': "There are paintings covering the walls."
+                       "To the west is The Parking Lot",
+        'PATHS': {
+            'WEST': "PARKING_LOT"
+        }
+    },
+    'HALLWAY': {
+        'NAME': "Main Hallway",
+        'DESCRIPTION': "This is the main hallway."
+                       "The Parking Lot is to the east,"
+                       "the quad is to the south, the gym is to the west"
+                       "and the library is to the north.",
+        'PATHS': {
+            'NORTH': "LIBRARY",
+            'EAST': "PARKING_LOT",
+            'SOUTH': "QUAD",
+            'WEST': "GYM",
+        }
+    },
+    'LIBRARY': {
+        'NAME': "The Library",
+        'DESCRIPTION': "This is the library. Books cover the walls"
+                       "and a little dog with glasses softly barks at you"
+                       "when you make too much noise. There's a path to"
+                       "the south back to the Main Hallway and a suspicious"
+                       "door to the north.",
+        'PATHS': {
+            'SOUTH': "HALLWAY",
+            'NORTH': "SUSPICIOUS_ROOM"
+        }
+    },
+    'SUSPICIOUS_ROOM': {}
+
 }
 
 # Other variables
