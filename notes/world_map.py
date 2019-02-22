4e4409be34005d1c5dbeb1b7faf1bca299569154
utvi <42383766+utvi@users.noreply.github.com>
2019-02-22 08:42:47 -0800

    owo

--- notes/world_map.py
+++ notes/world_map.py
@@ -1,16 +1,20 @@
 class Room(object):
-    def __init__(self, name, north=None, south=None, east=None, west=None, description=""):
+    def __init__(self, name, north=None, south=None, east=None, west=None, up=None, down=None, description=""):
         self.name = name
         self.north = north
         self.south = south
         self.east = east
         self.west = west
+        self.up = up
+        self.down = down
         self.description = description
 
 
 # Option 1
 R19A = Room("R19A")
-parking_lot = Room('The Parking Lot', None, R19A,)
+art_building = Room("Art Building")
+hallway = Room("Hallway")
+parking_lot = Room('The Parking Lot', None, R19A, art_building, hallway, None, None)
 
 R19A.north = parking_lot
 parking_lot.south = R19A
@@ -22,7 +26,12 @@ parking_lot.south = R19A
 art_building = Room('Art Building', None, parking_lot)
 
 art_building.west = parking_lot
+library = Room("Library")
+quad = Room("Quad")
+gym = Room("Gym")
+hallway = Room('The Hallway', library, quad, parking_lot, gym, None, None,)
 
-hallway = Room('The Hallway', None, parking_lot)
-
+hallway.north = library
+hallway.south = quad
 hallway.east = parking_lot
+hallway.east = gym
